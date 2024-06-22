e5b209b70937b555a9adb16a8f9aecca69193b2d
Sirojiddin Xidirnazarov <sirojiddinx1990@gmail.com>
2024-06-22 11:37:07 +0500

    if 19-20 done

--- if/if-20.js
+++ if/if-20.js
@@ -9,15 +9,15 @@ d = Math.floor((n % 100) / 10);
 f = n % 10;
 
 if(a > b && a > c && a > d && a > f){
-  console.log(a);
+  console.log(a, 5);
 } else if(b > a && b > c && b > d && b > f){
-  console.log(b);
+  console.log(b, 4);
 } else if(c > a && c > b && c > d && c > f){
-  console.log(c);
+  console.log(c, 3);
 } else if(d > a && d > b && d > c && d > f){
-  console.log(d);
+  console.log(d, 2);
 } else if(f > a && f > b && f > c && f > d){
-  console.log(f);
+  console.log(f, 1);
 }
 
 n = 36054;
@@ -29,15 +29,15 @@ d = Math.floor((n % 100) / 10);
 f = n % 10;
 
 if(a > b && a > c && a > d && a > f){
-  console.log(a);
+  console.log(a, 5);
 } else if(b > a && b > c && b > d && b > f){
-  console.log(b);
+  console.log(b, 4);
 } else if(c > a && c > b && c > d && c > f){
-  console.log(c);
+  console.log(c, 3);
 } else if(d > a && d > b && d > c && d > f){
-  console.log(d);
+  console.log(d, 2);
 } else if(f > a && f > b && f > c && f > d){
-  console.log(f);
+  console.log(f, 1);
 }
 
 n = 12345;
@@ -49,13 +49,13 @@ d = Math.floor((n % 100) / 10);
 f = n % 10;
 
 if(a > b && a > c && a > d && a > f){
-  console.log(a);
+  console.log(a, 5);
 } else if(b > a && b > c && b > d && b > f){
-  console.log(b);
+  console.log(b, 4);
 } else if(c > a && c > b && c > d && c > f){
-  console.log(c);
+  console.log(c, 3);
 } else if(d > a && d > b && d > c && d > f){
-  console.log(d);
+  console.log(d, 2);
 } else if(f > a && f > b && f > c && f > d){
-  console.log(f);
+  console.log(f, 1);
 }
